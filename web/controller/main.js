b7bda41127d0b757456202c37617ee83c01f045d
Damien Brun <makowildcat@gmail.com>
2018-04-22 19:17:58 -0400

    uglyyyy keep cominnng

--- web/controller/main.js
+++ web/controller/main.js
@@ -18,6 +18,11 @@
 var handCurrentUser = "";
 var buttonFirstTxt = "";
 var buttonSecondTxt = "";
+var firstVibrationSkipped = false;
+
+function setBackgroundImage(attribut, imageName, sizeWidth) {
+    return attribut + ' { background: url(img/' + imageName + '.png); background-size: ' + sizeWidth + 'px; } ';
+}
 
 // Initializes NotAlone.
 function NotAlone() {
@@ -85,25 +90,63 @@ NotAlone.prototype.signOut = function() {
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 NotAlone.prototype.onAuthStateChanged = function(user) {
   if (user) { // User is signed in!
+
+    // get the choosen hand from the database
     var handRef = this.database.ref('people/' + user.uid);
+    // continually listen to hand changes in database (in case of switching)
     handRef.on('value', function(snapshot) {
+      //var divContent = document.getElementById("content");
+      // make sure we clean the content of the page (screen)
+      // divContent.innerHTML = "";
+
+      // get the value of the hand (left or right)
       handCurrentUser = snapshot.val().hand;
-      var headerHandRef = document.getElementById("header-hand");
-      var buttonFirst = document.getElementById('btn-first');
-      var buttonSecond = document.getElementById('btn-second');
-      // document.getElementById("imageid").src="../template/save.png";
-      headerHandRef.innerHTML = handCurrentUser;
+
+      // if left
       if (handCurrentUser == "Left") {
+          // set values to save in database when the user vote
+          buttonFirstTxt = "Release";
+          buttonSecondTxt = "Grab";
+          $('#img-hand').css('transform', 'rotate(5deg)');
+      }
+      // else ... we suppose it is right
+      else {
+          // set values to save in database when the user vote
+          buttonFirstTxt = "Mash";
+          buttonSecondTxt = "Cook";
+          $('#img-hand').css('transform', 'rotate(-5deg)');
+      }
+      $('#img-hand').css('z-index', -1);
+
+      var newHTMLcontent = ''
+
+      //var headerHandRef = document.getElementById("header-hand");
+      //var buttonFirst = document.getElementById('btn-first');
+      //var buttonSecond = document.getElementById('btn-second');
+      // document.getElementById("imageid").src="../template/save.png";
+      //headerHandRef.innerHTML = handCurrentUser;
+      /*if (handCurrentUser == "Left") {
           buttonFirstTxt = "Grab";
           buttonSecondTxt = "Release";
       } else {
           buttonFirstTxt = "Mash";
           buttonSecondTxt = "Cook";
-      }
+      }*/
+
+      $('#img-first').css("height", screen.height/3*0.875 + "px");
+      $('#img-first').css("width", screen.height/3 + "px");
+      $('#img-second').css("height", screen.height/3*0.875 + "px");
+      $('#img-second').css("width", screen.height/3 + "px");
+      $('#img-hand').css("height", screen.height/7 + "px");
+      $('#img-hand').css("width", screen.height/7 + "px");
       var imageFirst = document.getElementById("img-first");
-      imageFirst.src = "img/" + buttonFirstTxt.toLowerCase() + ".png";
-      var css = '#img-first:active { background: url(img/' + buttonFirstTxt.toLowerCase() + '_press.png); background-size: ' + screen.height/2.5 + 'px; }';
-      css += '#img-second:active { background: url(img/' + buttonSecondTxt.toLowerCase() + '_press.png); background-size: ' + screen.height/2.5 + 'px; }';
+      //imageFirst.src = "img/" + buttonFirstTxt.toLowerCase() + ".png";
+      var css = setBackgroundImage("#img-first", buttonFirstTxt.toLowerCase(), screen.height/3);
+      css    += setBackgroundImage("#img-second", buttonSecondTxt.toLowerCase(), screen.height/3);
+      css    += setBackgroundImage("#img-first:active", buttonFirstTxt.toLowerCase() + '_press', screen.height/3);
+      css    += setBackgroundImage("#img-second:active", buttonSecondTxt.toLowerCase() + '_press', screen.height/3);
+      css    += '#img-hand { background: url(img/' + handCurrentUser.toLowerCase() + '_hand.gif); background-size: ' + screen.height/7 + 'px; } ';
+
       var style = document.createElement('style');
       if (style.styleSheet) {
           style.styleSheet.cssText = css;
@@ -111,19 +154,29 @@ NotAlone.prototype.onAuthStateChanged = function(user) {
           style.appendChild(document.createTextNode(css));
       }
       document.getElementsByTagName('head')[0].appendChild(style);
-      imageFirst.width = screen.height/2.5;
+      imageFirst.width = screen.height/3;
 
       var imageSecond = document.getElementById("img-second");
-      imageSecond.src= "img/" + buttonSecondTxt.toLowerCase() + ".png";
-      imageSecond.width = screen.height/2.5;
-
-
+      //imageSecond.src= "img/" + buttonSecondTxt.toLowerCase() + ".png";
+      imageSecond.width = screen.height/3;
+    });
+    var failureRef = this.database.ref('failure');
+    failureRef.on('value', function(snapshot) {
+      if (firstVibrationSkipped) {
+        window.navigator.vibrate(200);
+        console.log("it should vibrate");
+      } else {
+        firstVibrationSkipped = true;
+      }
     });
   } else { // User is signed out!
     // Show sign-in button, etc.
     window.location.href = "choseHand.html";
   }
 };
+
+
+
 
 // Returns true if user is signed-in. Otherwise false and displays a message.
 NotAlone.prototype.checkSignedInWithMessage = function() {
